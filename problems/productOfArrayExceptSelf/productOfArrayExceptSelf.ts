535ffb077bf4b698b30061cf658a0f7c7db96e83
Richard Hyesung O <orichh@gmail.com>
2022-01-04 22:47:55 -0500

    Complete with excalidraw

---
+++ problems/productOfArrayExceptSelf/productOfArrayExceptSelf.ts
@@ -0,0 +1,24 @@
+function productExceptSelf(nums: number[]) {
+  let results = [];
+  // establish results array with value of 1
+  for (let i = 1; i < nums.length; i++) {
+    results.push(1);
+  }
+  // iterate through nums array forwards, keeping track of product
+  for (let i = 1; i < nums.length; i++) {
+    // multiply by i with product to assign to i + 1
+    results[i] = nums[i - 1] * results[i - 1]; // nums[0] * results[0]
+    // results[2] = nums[1] * results[1] = 1 x 5 = 5
+    // results[3] = nums[2] * results[2] = 3 * 5 = 15
+  }
+
+  // iterate through nums array backwards, keeping track of product
+  let postfix = 1;
+  for (let i = nums.length - 1; i > -1; i--) {
+    // multiply by i with product to assign to i - 1
+    results[i] *= postfix;
+    postfix *= nums[i];
+  }
+
+  return results;
+}
