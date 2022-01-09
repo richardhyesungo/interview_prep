449d2fb48b32b153a85a11955d77c41746489190
Richard Hyesung O <orichh@gmail.com>
2022-01-08 21:55:42 -0500

    Complete, need to get more comfortable with regex

---
+++ problems/validPalindrome/validPalindrome.ts
@@ -0,0 +1,19 @@
+function isPalindrome(s: string): boolean {
+  // convert all letters to lowercase
+  // remove all non-alphanumeric characters
+  // includes both numbers and letters
+
+  // can do this in O(n) time with O(1) space
+  // iterate once, format
+  s = s.replace(/[^A-Z0-9]/gi, "");
+
+  // iterate again, compare from out to in, two pointers
+  for (var i = 0; i < s.length; i++) {
+    var left = s[i].toLowerCase();
+    var right = s[s.length - 1 - i].toLowerCase();
+    if (left !== right) {
+      return false;
+    }
+  }
+  return true;
+}
