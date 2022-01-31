d9f93adac58b41530daa41101bcb0822aaa05cec
Richard Hyesung O <orichh@gmail.com>
2022-01-31 12:30:42 -0500

    Complete rough heap implementation

--- javascript_fundamentals/data_structures/algorithmic/heap/binaryHeap.ts
+++ javascript_fundamentals/data_structures/algorithmic/heap/binaryHeap.ts
@@ -34,6 +34,18 @@ class MaxBinaryHeap {
     this.values.push(value); // O(1)
     this.bubbleUp(); // O(log n)
   }
+
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end!;
+      this.sinkDown();
+    }
+    return max;
+  }
+
+  // helper functions
   bubbleUp() {
     // initialize new element index and value
     let currentIdx: number = this.values.length - 1;
@@ -52,6 +64,49 @@ class MaxBinaryHeap {
       this.values[parentIdx] = element;
       this.values[currentIdx] = parent;
       currentIdx = parentIdx;
+    }
+  }
+
+  sinkDown() {
+    // after extracting an element from the root
+    // take the last element of the heap array
+    // place it at the top
+    // sink down (swap) this element if necessary until heap is correct
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild: any; // set to any for now just to get it to run
+      let rightChild;
+      let swap = null;
+
+      // swap if leftChildIdx exists and greater than current element
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+
+        // swap
+        if (leftChild > element) {
+          swap = leftChildIdx;
+        }
+      }
+
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+
+      if (swap === null) break;
+
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
     }
   }
 }
