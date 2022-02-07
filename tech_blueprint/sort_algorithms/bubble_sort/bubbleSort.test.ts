62aed138806879ec8199b5198077811dcb32a7ba
Richard Hyesung O <orichh@gmail.com>
2022-02-06 23:08:47 -0500

    Copy more tests for later for additional test cases

--- tech_blueprint/sort_algorithms/bubble_sort/bubbleSort.test.ts
+++ tech_blueprint/sort_algorithms/bubble_sort/bubbleSort.test.ts
@@ -9,5 +9,8 @@ describe("placeholder", () => {
     expect(5).toBe(5);
   });
 
-  expect(bubbleSort([4, 3, 2, 1])).toBe([1, 2, 3, 4]);
+  expect(bubbleSort([4, 3, 2, 1])).toEqual([1, 2, 3, 4]);
+  // expect(bubbleSort([4, 3, 2, 1])).toEqual([1, 2, 3, 4]);
+  // expect(bubbleSort([4, 3, 2, 1])).toEqual([1, 2, 3, 4]);
+  // expect(bubbleSort([4, 3, 2, 1])).toEqual([1, 2, 3, 4]);
 });
