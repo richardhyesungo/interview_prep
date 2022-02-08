cb029394c8c44fc2f0480ec2ed03ec7b36a63163
Richard Hyesung O <orichh@gmail.com>
2022-02-08 14:38:05 -0500

    Add interview questions to give short example code

---
+++ interview_prep_questions/what_is_asynchronous_programming.ts
@@ -0,0 +1,16 @@
+/**
+ * What is asynchronous programming and why is it important in JavaScript?
+ */
+
+/* Synchronous programming */
+// executed from top-to-bottom sequentially except conditionals and function calls
+// blocks on long-running tasks such as network requests and disk I/O
+console.log("this runs first");
+console.log("this runs second");
+console.log("this runs third");
+
+/* Asynchronous programming */
+setTimeout(() => {
+  console.log("async programming");
+}, 3000);
+console.log("async outside of setTimeout");
