2212baf04d02f4ad07a9807d6cb2f7b3f4f26b63
Richard Hyesung O <orichh@gmail.com>
2022-02-10 21:07:46 -0500

    Reformat and add leetcode 127

---
+++ javascript_fundamentals/primitives.js
@@ -0,0 +1,25 @@
+/**
+ * Primitive Types
+ * Data that is not an object and has no methods.
+ * All primitives are immutable
+ *
+ */
+
+// 1. string
+let bar = "baz";
+console.log(bar);
+bar.toUpperCase();
+console.log(bar);
+console.log(bar.toUpperCase());
+
+// 2. number
+
+// 3. bigint
+
+// 4. boolean
+
+// 5. undefined
+
+// 6. symbol
+
+// 7. null
